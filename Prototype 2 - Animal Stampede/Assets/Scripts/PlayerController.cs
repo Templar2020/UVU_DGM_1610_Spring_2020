76f458a9a2795b945e431a61fd50535d22ecc9f5
Preston Powell <preston.powell2020@gmail.com>
2021-02-01 17:20:56 -0700

    Created Prototype 2 Animal Stampede & Created Player Movement

---
+++ Prototype 2 - Animal Stampede/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float speed = 20.0f;
+    public float hInput;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        hInput = Input.GetAxis("Horizontal");
+        transform.Translate(Vector3.right * hInput * Time.deltaTime * speed);
+
+        if(transform.position.x < -10)
+        {
+         transform.position = new Vector3(-10, transform.position.y, transform.position.z); 
+        }
+        
+    }
+}
