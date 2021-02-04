d896e16c4654b342b21e2d3a1b62fbff7a535271
Preston Powell <preston.powell2020@gmail.com>
2021-02-03 17:21:28 -0700

    Up to Lesson 2.3

---
+++ Prototype 2 - Animal Stampede/Assets/Scripts/DestroyOutOfBounds.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyOutOfBounds : MonoBehaviour
+{
+    private float topBound = 35;
+    private float lowerBound = -15;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // Destroy(gameObject, 2);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(transform.position.z > topBound){
+            Destroy(gameObject);
+        }
+        else if(transform.position.z < lowerBound){
+            Destroy(gameObject);
+        }
+        
+    }
+}
--- Prototype 2 - Animal Stampede/Assets/Scripts/PlayerController.cs
+++ Prototype 2 - Animal Stampede/Assets/Scripts/PlayerController.cs
@@ -7,6 +7,10 @@ public class PlayerController : MonoBehaviour
     public float speed = 20.0f;
     public float hInput;
 
+    private float xRange  = 24;
+
+    public GameObject projectilePrefab;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -19,10 +23,20 @@ public class PlayerController : MonoBehaviour
         hInput = Input.GetAxis("Horizontal");
         transform.Translate(Vector3.right * hInput * Time.deltaTime * speed);
 
-        if(transform.position.x < -10)
+        if(transform.position.x < -xRange)
         {
-         transform.position = new Vector3(-10, transform.position.y, transform.position.z); 
+         transform.position = new Vector3(-xRange, transform.position.y, transform.position.z); 
         }
         
+        if(transform.position.x > xRange)
+        {
+         transform.position = new Vector3(xRange, transform.position.y, transform.position.z);
+        }
+
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            //Launch the projectile
+            Instantiate(projectilePrefab, transform.position, projectilePrefab.transform.rotation);
+        }
     }
 }
