0467b867c123c02aac22f6e7acc91b63993d48fc
Preston Powell <preston.powell2020@gmail.com>
2021-03-29 17:47:03 -0600

    Completed 4.3 PowerUp and CountDown

---
+++ Prototype 4/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private Rigidbody playerRb;
+    public float speed;
+    private GameObject focalPoint; 
+
+    public bool hasPowerup;
+    public float powerupStrength = 16.0f;
+
+    public GameObject powerupIndicator;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerRb = GetComponent<Rigidbody>();
+        focalPoint = GameObject.Find("FocalPoint");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float forwardInput = Input.GetAxis("Vertical");
+        playerRb.AddForce(focalPoint.transform.forward * speed * Time.deltaTime); 
+
+        powerupIndicator.transform.position = transform.position + new Vector3 (0, 0.5f,0);
+    }
+
+    // This block of code allows the player to pickup powerup item
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.CompareTag("Powerup"))
+        {
+            hasPowerup = true;
+            Destroy(other.gameObject);
+            Debug.Log("Powerup Collected!");
+
+            StartCoroutine(PowerupCountdownRoutine());
+
+            powerupIndicator.gameObject.SetActive(true);
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(collision.gameObject.CompareTag("Enemy") && hasPowerup)
+        {
+           // Gets the enemies rigidbody component so we can have accces to its physics properties
+           Rigidbody enemyRigidbody = collision.gameObject.GetComponent<Rigidbody>();
+           // Get the position of the enemy in relation to the player
+           Vector3 awayFromplayer = (collision.gameObject.transform.position - transform.position);
+           // Report player collison with pick up
+           Debug.Log("Player has collided with " + collision.gameObject + " with powerup set to "+ hasPowerup);
+           // On collison kicks enemy back 
+           enemyRigidbody.AddForce(awayFromplayer * powerupStrength, ForceMode.Impulse);     
+        }
+    }
+
+    IEnumerator PowerupCountdownRoutine()
+    {
+        yield return new WaitForSeconds(7); hasPowerup = false;
+        powerupIndicator.gameObject.SetActive(false);
+    }
+}
